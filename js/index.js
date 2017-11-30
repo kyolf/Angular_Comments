0095b822be74133357cae7d689e0e7c1514f1ad8
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-11-29 21:19:02 -0800

    added service

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 let comment_app = angular.module('comment_app', []);
 
-comment_app.service('Comment')
+comment_app.service('commentObj', function() {
+  this.comments = [];
 
-comment_app.controller('CommentController', function($scope) {
-  $scope.comments = [];
+  this.getComments = function() {
+    return this.comments;
+  };
+});
+
+comment_app.controller('CommentController', function($scope, commentObj) {
+  $scope.comments = commentObj.getComments();
 
   $scope.addComments = function() {
     $scope.comments.push({
