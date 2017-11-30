9049e22ce8f6efdf78e15afe2dc3ed29ce9960ea
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-11-29 18:48:29 -0800

    Got Comments to show up

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-let app = angular.module('app1', []);
+let comment_app = angular.module('comment_app', []);
+
+comment_app.service('Comment')
+
+comment_app.controller('CommentController', function($scope) {
+  $scope.comments = [];
+
+  $scope.addComments = function() {
+    $scope.comments.push({
+      text: $scope.newText,
+      likes: 0
+    });
+    $scope.newText = '';
+  };
+});
